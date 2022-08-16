7a49bc2609879c005dbb8f2ffee1f46a6b7cdd01
raghav <raghav20csu084@ncuindia.edu>
2022-08-16 13:03:33 +0530

    First Commit

---
+++ src/com/company/DuplicatesInArray.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class DuplicatesInArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++)
+            arr[i] = sc.nextInt();
+
+//        ArrayList<Integer> ds = findDuplicates(arr);
+    }
+
+//    private static ArrayList<Integer> findDuplicates(int[] arr) {
+//        int[] freq = new int[arr.length];
+//        ArrayList<Integer> ds = new ArrayList<>();
+//        for(int i: arr){
+//            if(freq[i]!=0 && )
+//                freq[i] +=1;
+//            else
+//                freq[i] +=;
+//        }
+//    }
+}
